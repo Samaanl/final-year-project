3589025b01b4f5658bed78a69ffb3e7c99fa291b
Samaan Sayed <nasifasayed@gmail.com>
2024-11-28 16:36:27 +0530

    added dnd features, this features are in drag, drop, comb-drag-drop-jsx and Ex file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-
+import Ex from "./Ex.jsx";
 export default function App() {
   return (
     <div className=" h-screen flex flex-col gap">
       <div className="bg-white h-12 ">top bar</div>
-      <div className=" h-full flex  gap-0.5">
-        <div className="bg-gray-200 flex-1 ">main canvas</div>
-        <div className="bg-gray-300 w-52 ">side bar</div>
-      </div>
+      <Ex />
     </div>
   );
 }
---
+++ src/Ex.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Comb from "./comb-drag-and-drop-jsx.jsx";
+const Ex = () => {
+  return (
+    <>
+      <Comb />
+    </>
+  );
+};
+
+export default Ex;
