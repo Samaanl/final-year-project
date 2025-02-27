d9fb9fbe37e992ec87793b0c3bde9fb452860d1d
Ankush703-web <naikankush067@gmail.com>
2025-02-27 10:57:05 +0530

    fixed some issues

--- src/temp/build/sketch/sketch.ino.cpp
+++ src/temp/build/sketch/sketch.ino.cpp
@@ -2,20 +2,17 @@
 #line 1 "E:\\Ty_Project\\project\\integrated_backend\\final-year-project\\src\\temp\\sketch\\sketch.ino"
 #line 1 "E:\\Ty_Project\\project\\integrated_backend\\final-year-project\\src\\temp\\sketch\\sketch.ino"
 void setup();
-#line 6 "E:\\Ty_Project\\project\\integrated_backend\\final-year-project\\src\\temp\\sketch\\sketch.ino"
+#line 5 "E:\\Ty_Project\\project\\integrated_backend\\final-year-project\\src\\temp\\sketch\\sketch.ino"
 void loop();
 #line 1 "E:\\Ty_Project\\project\\integrated_backend\\final-year-project\\src\\temp\\sketch\\sketch.ino"
 void setup() {
   pinMode(13, OUTPUT);
-  pinMode(12,OUTPUT);
 }
 
 void loop() {
-  digitalWrite(13, HIGH);
-  digitalWrite(12,LOW);  
+  digitalWrite(13, HIGH);  
   delay(1000);                      
-  digitalWrite(13, LOW); 
-  digitalWrite(12,HIGH);  
+  digitalWrite(13, LOW);   
   delay(1000);                      
 }
   
--- src/temp/sketch/sketch.ino
+++ src/temp/sketch/sketch.ino
@@ -1,14 +1,11 @@
 void setup() {
   pinMode(13, OUTPUT);
-  pinMode(12,OUTPUT);
 }
 
 void loop() {
-  digitalWrite(13, HIGH);
-  digitalWrite(12,LOW);  
+  digitalWrite(13, HIGH);  
   delay(1000);                      
-  digitalWrite(13, LOW); 
-  digitalWrite(12,HIGH);  
+  digitalWrite(13, LOW);   
   delay(1000);                      
 }
   
